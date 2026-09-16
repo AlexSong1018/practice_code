a29adf981a95e2b1f534cc8b7c2e60d951dbc662
AlexSong1018 <qi_666999@qq.com>
2026-09-16 08:49:39 +0800

    温度转换

---
+++ TempConvert.py
@@ -0,0 +1,11 @@
+TempStr=input("输入一个温度值，以F或C结尾：")
+l=TempStr[-1]
+inputTemp=int(TempStr[0:-1])
+if l=='C':
+    F=inputTemp*1.8+32
+    print("转换为F为：",F,"F")
+elif l=='F':
+    C=(inputTemp-32)/1.8
+    print("转换为C为：",C,"C")
+else:
+    print("格式错误，请重新输入")
